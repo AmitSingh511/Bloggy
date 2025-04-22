3bee2ae607da25489c07ebe092134e849608afe2
AmitSingh511 <sandhuamit4217@gmail.com>
2025-04-23 05:27:09 +0530

    , Linking html form to the database.

--- src/create.php
+++ src/create.php
@@ -1,6 +1,3 @@
-<?php
-include "config.php";
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,21 +12,70 @@ include "config.php";
         <div class="flex justify-center items-center mt-40">
             <form method="post" enctype="multipart/form-data" class="flex flex-col gap-4 border-2 w-100 p-10">
                 <label for="title">Title</label>
-                <input type="text" id="title" name="title" class="input" required >
+                <input type="text" id="title" name="title" class="border-2 border-gray-600" required >
                 <label for="author">Author</label>
-                <input type="text" id="author" name="author" required>
+                <input type="text" id="author" name="author" class="border-2 border-gray-600" required>
                 <label for="content">Content</label>
-                <select name="content[]" id="content" multiple required>
+                <select name="content[]" id="content" class="border-2 border-gray-600" multiple required>
                     <option value="Technology">Technology</option>
                     <option value="Education">Education</option>
                     <option value="Lifestyle">Lifestyle</option>
                     <option value="Health">Health</option>
                     <option value="Other">Other</option>
                 </select> <br><br>
-                <input type="file" id="upload" name="upload" required>
-                <button type="submit">Upload</button>
+                <input type="file" id="upload" name="upload" class="border-2 border-gray-600" required>
+                <button type="submit" name="submit" class=" bg-green-500 p-1 rounded-xl">Upload</button> 
             </form>
     </div>
 </body>
 
 </html>
+
+<?php
+include "config.php";
+if(isset($_POST['submit']))
+{
+$title=$_POST['title'];
+$author=$_POST['author'];
+$content=implode(",",$_POST['content']);
+$target_dir='../uploads/';
+$target_file=$target_dir.basename($_FILES['upload']['name']);//upload/image.jpg
+$file_type=strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$ext=['jpg','jpeg','png','gif','pdf'];
+$ok=0;
+$sql="insert into users (title,content,author,image) values ('$title','$author','$content','$target_file')";
+   if($_FILES["upload"]["size"]>5000000) //file size validation
+   {
+    echo "file size should be 5MB or less";
+    $ok=1;
+   }
+   elseif(!in_array($file_type,$ext))
+   {
+    echo "sorrry!only jpg,jpeg,png,gif and pdf is supported";
+    $ok=1;
+   }
+    elseif ($ok==0) {
+        if(move_uploaded_file($_FILES['upload']['tmp_name'],$target_file))
+        {
+            echo "";
+        }
+        else
+        {
+            echo 'sorry there was an error uploading the file';
+        }
+    }
+    else
+    {
+        echo "sorry, theree was an issue in uploading a file, try agian later";
+    }
+
+}
+if(mysqli_query($conn,$sql))
+{
+    echo "records inserted successfully";
+}
+else
+{
+    die ('error! try again later');
+}
+?>
